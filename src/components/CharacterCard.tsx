c4fdbda8ff9fd420d3e341c07d65677c8c60a140
Ali Feruz <147985313+AliFeruz@users.noreply.github.com>
2024-01-19 09:38:00 +0100

    edited Image component

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -11,7 +11,7 @@ const CharacterCard = ({character}: Props) => {
   return (
     <div className="flex-col mx-3 items-center border border-orange-400 justify-center dark:bg-gradient-to-t dark:from-[#1c047e] dark:to-fuchsia-800 p-4 shadow-text rounded-md shadow-lg">
         <div className="items-center justify-center rounded-md">
-        <Image src={character.image} className="w-auto h-auto" alt="character image" />
+        <Image src={character.image} className="w-[300px] h-[300px]" alt="character image" />
         </div>
       <div className="mt-4 border-t border-orange-500 pt-4">
       <h1 className="text-xl text-center text-orange-400 font-bold mb-2">{character.name}</h1>
